7384592d0ad8e0e34ea2eb7bbc80f74ab472bb32
pjmakwana92 <pjmakwana92@gmail.com>
2023-09-30 12:11:23 +0530

    30/9/23

---
+++ ex4.c
@@ -0,0 +1,17 @@
+//write a program to calculate income , tack monthly income and check yearly income greeter then 5 lakh 5% .
+#include<stdio.h>
+void main()
+{
+    int m_sal, y_sal=0;
+    printf("Enter your monthly salary = ");
+    scanf("%d",&m_sal);
+
+    printf("your monthly salary is %d",m_sal);
+    y_sal=(12*m_sal);
+    if (y_sal>500000)
+    {
+        printf("your yearly salary is greeter than 5 lakh");
+        y_sal=y_sal-(y_sal(5/100));
+    }
+    printf("your salary is %f",y_sal);
+}
